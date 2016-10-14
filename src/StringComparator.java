07513b1968cb3f39b8da290c30ada5781656ca05
user <user@FC12>
2016-10-14 09:36:21 +0800

    Homework 3

--- src/StringComparator.java
+++ src/StringComparator.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class StringComparator {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		Boolean flag = true;
 		String string1, string2;
-		System.out.print("Enter a string 1:");
-		string1 = scanner.next();
-		System.out.print("Enter a string 2:");
-		string2 = scanner.next();
-		if(string1.compareToIgnoreCase(string2)==0)
-			System.out.print("The two strings are the same.");
-		else
-			System.out.print("The two strings are not the same.");
+		do {
+			System.out.print("Enter a string 1:");
+			string1 = scanner.next();
+			System.out.print("Enter a string 2:");
+			string2 = scanner.next();
+			if (string1.compareToIgnoreCase(string2) == 0) {
+				System.out.println("The two strings are the same.");
+				flag = false;
+			} else
+				System.out.println("The two strings are not the same.");
+		} while (flag);
+		scanner.close();
 	}
 }
